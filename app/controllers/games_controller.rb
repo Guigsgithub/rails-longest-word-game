249740fe8373048c8b83eb353310d96e8f429a0f
Guillaume LEFAY <guillaume.lefay@gmail.com>
2020-11-09 16:59:33 +0100

    wip

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,11 +1,33 @@
 class GamesController < ApplicationController
   def new
-    def generate_grid(grid_size)
-      Array.new(grid_size) { ('A'..'Z').to_a.sample }
-    end
     @letters = generate_grid(9)
   end
 
   def score
+    if included?(params[:word])
+      if english_word?(params[:word])
+      @result = "<b>Congratulations!</b> #{params[:word].upcase} is a valid English word"
+      else
+      @result = "Sorry but #{params[:word].upcase} doesn't seem to be a valid English word..."
+      end
+    else
+      @result = "Sorry but #{params[:word].upcase} can't be build out of #{@letters}"
+    end
+  end
+
+  private
+
+  def generate_grid(grid_size)
+    Array.new(grid_size) { ('A'..'Z').to_a.sample }
+  end
+
+  def included?(word)
+    # word.chars.all? { |letter| word.count(letter) <= @letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
   end
 end
